44f90e7f4cc63e5c61adb3ebd78e3a9ac65d6e63
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2025-09-05 14:03:37 +0400

    field displacement map

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -2,7 +2,7 @@
 precision mediump float; // Указываем точность для float
 precision mediump sampler2D; // Указываем точность для sampler2D
 
-in vec2 vUV;
+in vec2 vUV; // [0,1]
 out vec4 outColor;
 
 uniform sampler2D pixelTexture;
@@ -12,12 +12,12 @@ uniform float x;
 void main() {
     vec4 texColor = texture(pixelTexture, vUV);
 
-    float radius = 0.4;
-
-    vec2 delta_center = vUV - vec2(x, 0.5);
+    vec2 delta_center = vUV - vec2(x, 0.5); // [-x,1-x] / [-0.5,0.5]
     delta_center.y = delta_center.y / ratio;
 
-    float red = 1.0 - length(delta_center) / radius;
+    float delta_len = length(delta_center);
+    float brightness = 0.1 / delta_len;
+    vec2 direction = delta_center / delta_len + 1.0; // [0,1]
 
-    outColor = vec4(red * texColor.r, 0, 0, 1);
+    outColor = vec4(brightness * direction * texColor.r, 0, 1); // [0,1]
 }
